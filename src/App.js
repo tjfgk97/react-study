5302615d77c33f2b5f1e11511609885a42247861
tjfgk97 <tjfgk97@gmail.com>
2024-08-27 00:56:22 +0900

    state 변수대신사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  let [글제목, 글제목변경] = useState('남자 코트 추천');
+
+  let posts = '강남 고기 맛집';
+  return (                      //html처럼 생긴 jsx 문법
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h4>{ 글제목 }</h4>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
